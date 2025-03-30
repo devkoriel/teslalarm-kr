85def98a42949a42145416ee5e965cdd1027597a
Jinsoo Heo <dev.koriel@gmail.com>
2025-03-30 18:25:38 +0900

    Use telegram webhook

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,6 +1,6 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
-from config import TELEGRAM_BOT_TOKEN
+from config import TELEGRAM_BOT_TOKEN, WEBHOOK_LISTEN, WEBHOOK_PORT, WEBHOOK_URL
 from telegram_bot.command_handler import change_language, handle_text
 from utils.logger import setup_logger
 
@@ -10,6 +10,17 @@ logger = setup_logger()
 def create_application():
     app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     app.add_handler(CommandHandler("language", change_language))
-    # /keywords 핸들러는 제거되었습니다.
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
     return app
+
+
+def run_webhook(app):
+    # 텔레그램 서버에 webhook URL 설정
+    app.bot.set_webhook(WEBHOOK_URL)
+    # webhook 방식으로 서버 실행
+    app.run_webhook(
+        listen=WEBHOOK_LISTEN,
+        port=WEBHOOK_PORT,
+        url_path=TELEGRAM_BOT_TOKEN,  # URL 경로에 봇 토큰 사용
+        webhook_url=WEBHOOK_URL,
+    )
